206061480a7e21709b3d3916e6c4f8e949f2cb24
XavierJarro <xa_cx4_vi@hotmail.com>
2019-12-18 23:52:28 -0500

    Creación de Clases
    Clase Socio y Clase Cuenta

---
+++ SGCA/src/main/java/ec/edu/ups/modelo/Cuenta.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Starman
+ */
+public class Cuenta {
+    
+    private int codigo;
+    private String numeroCuenta;
+    private Date fechaRegistro;
+    private String estado;
+    private Socio socio;
+
+    public Cuenta() {
+    }
+
+    public Cuenta(int codigo, String numeroCuenta, Date fechaRegistro, String estado, Socio socio) {
+        this.codigo = codigo;
+        this.numeroCuenta = numeroCuenta;
+        this.fechaRegistro = fechaRegistro;
+        this.estado = estado;
+        this.socio = socio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Socio getSocio() {
+        return socio;
+    }
+
+    public void setSocio(Socio socio) {
+        this.socio = socio;
+    }
+    
+    
+    
+}
