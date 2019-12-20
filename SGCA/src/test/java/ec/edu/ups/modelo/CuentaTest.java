03494ba2524b19a0c906b14672629db00cad7ef5
NikoAP77 <nikoap77@gmail.com>
2019-12-19 22:54:59 -0500

    Creación del Test
    Test de la clase Cuenta

---
+++ SGCA/src/test/java/ec/edu/ups/modelo/CuentaTest.java
@@ -0,0 +1,196 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author USER
+ */
+public class CuentaTest {
+    
+    private Cuenta c1;
+    private Socio s1;
+    public CuentaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() throws ParseException{
+        DateFormat format = new SimpleDateFormat("dd/mm/yyyy"); // Creamos un fomato de fecha
+        Date fecha = format.parse("25/07/2017");
+        System.out.println(fecha);
+        Date fechaN = format.parse("20/04/1994");
+        System.err.println(fechaN);
+        Date fechaI = format.parse("10/01/2016");
+        System.err.println(fechaI);
+               
+        
+        
+        
+        s1 = new Socio("Activo", fechaI, 12.666, 1, "0703021287", "Jordan", "Murillo", fechaN, "0980792708", "Av. Loja" );
+    
+        c1 = new Cuenta(1, "789654321" , fecha, "Activo",s1);
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getCodigo method, of class Cuenta.
+     */
+    @Test
+    public void testGetCodigo() {
+        System.out.println("getCodigo");
+        Cuenta instance = new Cuenta();
+        int expResult = 0;
+        int result = instance.getCodigo();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCodigo method, of class Cuenta.
+     */
+    @Test
+    public void testSetCodigo() {
+        System.out.println("setCodigo");
+        int codigo = 0;
+        Cuenta instance = new Cuenta();
+        instance.setCodigo(codigo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getNumeroCuenta method, of class Cuenta.
+     */
+    @Test
+    public void testGetNumeroCuenta() {
+        System.out.println("getNumeroCuenta");
+        Cuenta instance = new Cuenta();
+        String expResult = "";
+        String result = instance.getNumeroCuenta();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNumeroCuenta method, of class Cuenta.
+     */
+    @Test
+    public void testSetNumeroCuenta() {
+        System.out.println("setNumeroCuenta");
+        String numeroCuenta = "";
+        Cuenta instance = new Cuenta();
+        instance.setNumeroCuenta(numeroCuenta);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getFechaRegistro method, of class Cuenta.
+     */
+    @Test
+    public void testGetFechaRegistro() {
+        System.out.println("getFechaRegistro");
+        Cuenta instance = new Cuenta();
+        Date expResult = null;
+        Date result = instance.getFechaRegistro();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setFechaRegistro method, of class Cuenta.
+     */
+    @Test
+    public void testSetFechaRegistro() {
+        System.out.println("setFechaRegistro");
+        Date fechaRegistro = null;
+        Cuenta instance = new Cuenta();
+        instance.setFechaRegistro(fechaRegistro);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEstado method, of class Cuenta.
+     */
+    @Test
+    public void testGetEstado() {
+        System.out.println("getEstado");
+        Cuenta instance = new Cuenta();
+        String expResult = "";
+        String result = instance.getEstado();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEstado method, of class Cuenta.
+     */
+    @Test
+    public void testSetEstado() {
+        System.out.println("setEstado");
+        String estado = "";
+        Cuenta instance = new Cuenta();
+        instance.setEstado(estado);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSocio method, of class Cuenta.
+     */
+    @Test
+    public void testGetSocio() {
+        System.out.println("getSocio");
+        Cuenta instance = new Cuenta();
+        Socio expResult = null;
+        Socio result = instance.getSocio();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSocio method, of class Cuenta.
+     */
+    @Test
+    public void testSetSocio() {
+        System.out.println("setSocio");
+        Socio socio = null;
+        Cuenta instance = new Cuenta();
+        instance.setSocio(socio);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
